33084c536ec0ceb7f9605fc5b94d08ddd74f8363
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-06 20:00:24 +0200

    Added routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+
+// components
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h2>Hey</h2>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
