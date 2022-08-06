8dcdf9a399afa0abedb639de46003f7f98da277f
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-06 20:21:59 +0200

    Added error and about pages.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae,
+        cupiditate nihil! Iste deleniti blanditiis natus fuga fugit delectus
+        error, ea consequuntur porro eum. Dignissimos ullam non, cumque iusto
+        autem dolorem corporis eligendi deleniti sint, aperiam a, aut ipsam
+        exercitationem accusamus.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
